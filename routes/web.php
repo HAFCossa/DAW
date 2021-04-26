e5dd424564ced220e64240f247679db9a28ff366
Dercio Pale <31033591+derciopale@users.noreply.github.com>
2021-04-27 00:55:28 +0200

    Definindo rota da musica

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\MusicaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,5 @@ Route::get('/', function () {
 
 
 Route::resource('cliente', ClienteController::class);
+
+Route::resource('musica', MusicaController::class);
